568d9ebc1885b7a16458c22c1082568b6e07f902
Krish <khambhatikrish@gmail.com>
2026-05-31 21:21:06 +0530

    create login thunk

--- frontend/src/features/auth/state/Thunk/authLoginThunk.jsx
+++ frontend/src/features/auth/state/Thunk/authLoginThunk.jsx
@@ -1,3 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"
+import { axiosInstance } from "../../../../config/axiosInstance"
 
 
+export let loginThunk = createAsyncThunk("auth/login", async (credentials, thunkError) => {
+    try {
 
+        let res = await axiosInstance.post("auth/login", credentials)
+
+        return res.data
+
+    } catch (error) {
+        return thunkError.rejectWithValue("error in login Thunk-->", error)
+    }
+})
